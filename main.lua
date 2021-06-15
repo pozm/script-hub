351de1a536b53671de6da2c1b8adf33430d4ffc3
kanner <74814256+kanenr@users.noreply.github.com>
2021-06-14 22:24:32 -0700

    Update main.lua
    cringe variables (lol)

--- main.lua
+++ main.lua
@@ -1,5 +1,13 @@
 local Commit = 2
 repeat wait() until game:IsLoaded()
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
+local TweenService = game:GetService("TweenService")
+local StarterGui = game:GetService("StarterGui")
+local Players = game:GetService("Players")
+local CoreGui = game:GetService("CoreGui")
+local Settings = {}
+
 if game and game.Workspace and game.ReplicatedStorage and game.Players and game.Stats and game.Chat and game.CoreGui and game.StarterGui  and game.ReplicatedFirst and game.StarterPlayer and game.StarterPack and not game.Workspace.FilteringEnabled == false then
     print("Welcome to the script fellas!")
     print("Script Hub v".. tostring(Commit) .." loaded!")
