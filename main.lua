55a7a4cf07b6b13c0de42bf0b58e66130d23ac06
safazi <safazi@airmail.cc>
2021-06-15 01:06:31 -0500

    spacing

--- main.lua
+++ main.lua
@@ -59,10 +59,12 @@ end
 
 local TrollUI = Instance.new("ScreenGui")
     if syn then syn.protect_gui(TrollUI) TrollUI.Parent = CoreGui elseif gethui then TrollUI.Parent = gethui() else TrollUI.Parent = CoreGui end 
+
 local Frame = Instance.new("Frame")
     Frame.Parent = TrollUI
     Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     Frame.Size = UDim2.new(0, 300, 0, 200)
+
 local TextLabel = Instance.new("TextLabel")
     TextLabel.Parent = Frame
     TextLabel.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
@@ -74,18 +76,22 @@ local TextLabel = Instance.new("TextLabel")
     TextLabel.TextSize = 16
     TextLabel.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
     TextLabel.TextStrokeTransparency = 0.8
+
 local Frame_2 = Instance.new("Frame")
     Frame_2.Parent = Frame
     Frame_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     Frame_2.Size = UDim2.new(1, 0, 1, 0)
+
 local UIPadding = Instance.new("UIPadding")
     UIPadding.Parent = Frame_2
     UIPadding.PaddingTop = UDim.new(0, 25)
+
 local UIGridLayout = Instance.new("UIGridLayout")
     UIGridLayout.Parent = Frame_2
     UIGridLayout.SortOrder = Enum.SortOrder.LayoutOrder
     UIGridLayout.CellPadding = UDim2.new(0, 0, 0, 0)
     UIGridLayout.CellSize = UDim2.new(0, 100, 0, 35)
+
 local createToggle = function(name, call)
     local button = setmetatable({}, {
         __index = self
@@ -100,6 +106,7 @@ local createToggle = function(name, call)
         TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
         TextButton.TextSize = 14.000
         TextButton.Text = name
+    
     local Frame_3 = Instance.new("Frame")
         Frame_3.Parent = TextButton
         Frame_3.AnchorPoint = Vector2.new(1, 0)
@@ -125,4 +132,5 @@ end
 local funnyToggle = createToggle("funny", function(Value)
     print(tostring(Value))
 end)
+
 end
