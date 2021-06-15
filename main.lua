d4a5da363de682ee3fb857fa5be31fee88062647
kanner <74814256+kanenr@users.noreply.github.com>
2021-06-14 22:34:26 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
-local Commit = 2.1
+local Commit = 2.2
+
 repeat wait() until game:IsLoaded()
 
 local Services = setmetatable({}, {
@@ -15,6 +16,24 @@ local Players           = Services.Players
 local CoreGui           = Services.CoreGui
 
 local Settings = {}
+
+local TrollUI = Instance.new("ScreenGui")
+if syn then syn.protect_gui(TrollUI) TrollUI.Parent = CoreGui elseif gethui then TrollUI.Parent = gethui() else TrollUI.Parent = CoreGui end 
+local Frame = Instance.new("Frame")
+    Frame.Parent = TrollUI
+    Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    Frame.Size = UDim2.new(0, 300, 0, 200)
+local TextLabel = Instance.new("TextLabel")
+    TextLabel.Parent = Frame
+    TextLabel.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+    TextLabel.Size = UDim2.new(1, 0, 0, 25)
+    TextLabel.ZIndex = 2
+    TextLabel.Font = Enum.Font.Roboto
+    TextLabel.Text = "Troll Face GUI"
+    TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+    TextLabel.TextSize = 16
+    TextLabel.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+    TextLabel.TextStrokeTransparency = 0.8
 
 if game and game.Workspace and game.ReplicatedStorage and game.Players and game.Stats and game.Chat and game.CoreGui and game.StarterGui  and game.ReplicatedFirst and game.StarterPlayer and game.StarterPack and not game.Workspace.FilteringEnabled == false then
     print("Welcome to the script fellas!")
