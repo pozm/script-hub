5e20015876049c7b1fbecf31eca82e1263a99621
David <48262695+unholytwig@users.noreply.github.com>
2021-06-15 01:31:46 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,19 @@
-local Commit = 2
+local Commit = "2.1"
 repeat wait() until game:IsLoaded()
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local ReplicatedFirst = game:GetService("ReplicatedFirst")
-local TweenService = game:GetService("TweenService")
-local StarterGui = game:GetService("StarterGui")
-local Players = game:GetService("Players")
-local CoreGui = game:GetService("CoreGui")
+
+local Services = setmetatable({}, {
+    __index = function(self, key)
+        return game:GetService(key)
+    end
+})
+
+local ReplicatedStorage = Services.ReplicatedStorage
+local ReplicatedFirst   = Services.ReplicatedFirst
+local TweenService      = Services.TweenService
+local StarterGui        = Services.StarterGui
+local Players           = Services.Players
+local CoreGui           = Services.CoreGui
+
 local Settings = {}
 
 if game and game.Workspace and game.ReplicatedStorage and game.Players and game.Stats and game.Chat and game.CoreGui and game.StarterGui  and game.ReplicatedFirst and game.StarterPlayer and game.StarterPack and not game.Workspace.FilteringEnabled == false then
