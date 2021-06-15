df03835c8a9e9cf88a05df01d4a661ecb8b87d91
kanner <74814256+kanenr@users.noreply.github.com>
2021-06-14 22:19:19 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
-local Commit = "1"
+local Commit = 2
+repeat wait() until game:IsLoaded()
 if game and game.Workspace and game.ReplicatedStorage and game.Players and game.Stats and game.Chat and game.CoreGui and game.StarterGui  and game.ReplicatedFirst and game.StarterPlayer and game.StarterPack and not game.Workspace.FilteringEnabled == false then
     print("Welcome to the script fellas!")
-    print("Script Hub v"..Commit.." loaded!")
+    print("Script Hub v".. tostring(Commit) .." loaded!")
 end
