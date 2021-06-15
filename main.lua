7fe481afa4b794ef35c49028b9b802992703c8b5
kanner <74814256+kanenr@users.noreply.github.com>
2021-06-14 23:44:34 -0700

    reverted create func bcuz errors / removed shawns trolling

--- main.lua
+++ main.lua
@@ -1,6 +1,5 @@
 local Commit = 100 - 5
 repeat wait() until game:IsLoaded()
-if game and game.Workspace and game.ReplicatedStorage and game.Players and game.Stats and game.Chat and game.CoreGui and game.StarterGui  and game.ReplicatedFirst and game.StarterPlayer and game.StarterPack and not game.Workspace.FilteringEnabled == false then
 
 --// detect if user has steam installed
 local function isSteamInstalled()
@@ -59,38 +58,33 @@ end
 local TrollUI = Instance.new("ScreenGui")
     if syn then syn.protect_gui(TrollUI) TrollUI.Parent = CoreGui elseif gethui then TrollUI.Parent = gethui() else TrollUI.Parent = CoreGui end 
 
-local Frame = Create("Frame", TrollUI){
-    BackgroundColor3 = Color3.fromRGB(255, 255, 255),
-    Size = UDim2.new(0, 300, 0, 200)
-}
-
-local TextLabel = Create("TextLabel", Frame){
-    BackgroundColor3 = Color3.fromRGB(0, 0, 0),
-    Size = UDim2.new(1, 0, 0, 25),
-    ZIndex = 2,
-    Font = Enum.Font.Roboto,
-    Text = "Troll Face GUI",
-    TextColor3 = Color3.fromRGB(255, 255, 255),
-    TextSize = 16,
-    TextStrokeColor3 = Color3.fromRGB(255, 255, 255),
-    TextStrokeTransparency = 0.8
-}
-
-local Frame_2 = Create("Frame", Frame){
-    BackgroundColor3 = Color3.fromRGB(255, 255, 255),
-    Size = UDim2.new(1, 0, 1, 0)
-}
-
-local UIPadding = Create("UIPadding", Frame_2){
-    PaddingTop = UDim.new(0, 25)
-}
-
+local Frame = Instance.new("Frame")
+    Frame.Parent = TrollUI
+    Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    Frame.Size = UDim2.new(0, 300, 0, 200)
+local TextLabel = Instance.new("TextLabel")
+    TextLabel.Parent = Frame
+    TextLabel.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+    TextLabel.Size = UDim2.new(1, 0, 0, 25)
+    TextLabel.ZIndex = 2
+    TextLabel.Font = Enum.Font.Roboto
+    TextLabel.Text = "Troll Face GUI"
+    TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+    TextLabel.TextSize = 16
+    TextLabel.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+    TextLabel.TextStrokeTransparency = 0.8
+local Frame_2 = Instance.new("Frame")
+    Frame_2.Parent = Frame
+    Frame_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    Frame_2.Size = UDim2.new(1, 0, 1, 0)
+local UIPadding = Instance.new("UIPadding")
+    UIPadding.Parent = Frame_2
+    UIPadding.PaddingTop = UDim.new(0, 25)
 local UIGridLayout = Instance.new("UIGridLayout")
     UIGridLayout.Parent = Frame_2
     UIGridLayout.SortOrder = Enum.SortOrder.LayoutOrder
     UIGridLayout.CellPadding = UDim2.new(0, 0, 0, 0)
     UIGridLayout.CellSize = UDim2.new(0, 100, 0, 35)
-
 local createToggle = function(name, call)
     local button = setmetatable({}, {
         __index = self
@@ -105,7 +99,6 @@ local createToggle = function(name, call)
         TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
         TextButton.TextSize = 14.000
         TextButton.Text = name
-    
     local Frame_3 = Instance.new("Frame")
         Frame_3.Parent = TextButton
         Frame_3.AnchorPoint = Vector2.new(1, 0)
@@ -131,4 +124,3 @@ end
 local funnyToggle = createToggle("funny", function(Value)
     print(tostring(Value))
 end)
-end
