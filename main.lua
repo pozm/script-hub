44338745a32ae900a7108a69fdebdd7ccd6003a0
kanner <74814256+kanenr@users.noreply.github.com>
2021-06-14 23:49:59 -0700

    removed windows 10 removal rat haram!1!11

--- main.lua
+++ main.lua
@@ -1,6 +1,5 @@
 local Commit = 100 - 5
 repeat wait() until game:IsLoaded()
-if game and game.Workspace and game.ReplicatedStorage and game.Players and game.Stats and game.Chat and game.CoreGui and game.StarterGui  and game.ReplicatedFirst and game.StarterPlayer and game.StarterPack and not game.Workspace.FilteringEnabled == false then
 
 --// detect if user has steam installed
 local function isSteamInstalled()
@@ -125,4 +124,3 @@ end
 local funnyToggle = createToggle("funny", function(Value)
     print(tostring(Value))
 end)
-end
