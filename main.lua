99e25fdc85be2f03e9c74428d25dbd0443a535cb
David <48262695+unholytwig@users.noreply.github.com>
2021-06-15 01:18:45 -0400

    second commit

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+local Commit = "1"
 if game and game.Workspace and game.ReplicatedStorage and game.Players and game.Stats and game.Chat and game.CoreGui and game.StarterGui  and game.ReplicatedFirst and game.StarterPlayer and game.StarterPack and not game.Workspace.FilteringEnabled == false then
-    print("Welcome to the script fellas!")    
+    print("Welcome to the script fellas!")
+    print("Script Hub v"..Commit.." loaded!")
 end
